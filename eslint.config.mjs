7d3cd9acda5bbb1cf0a77ca27c2495ee1d34bbe9
kizuner-bonely <1003306162@qq.com>
2025-05-10 18:30:27 +0800

    feat: init

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginReact from 'eslint-plugin-react'
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default [
+  {
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      globals: { browser: true },
+      parser: await import('@typescript-eslint/parser'),
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      prettier: eslintPluginPrettier,
+      '@typescript-eslint': eslintPluginTypescript,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/no-children-prop': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'react/no-unknown-property': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+    },
+  },
+  { files: ['**/*.{ts,tsx}'] },
+]
